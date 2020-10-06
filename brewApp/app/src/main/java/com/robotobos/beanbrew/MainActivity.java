12a9632e8190c7e3cd7ddfaaa73caf93eaa11c72
mjb484 <robotobos@protonmail.com>
2020-10-05 20:40:30 -0400

    added socket code for android

--- brewApp/app/src/main/java/com/robotobos/beanbrew/MainActivity.java
+++ brewApp/app/src/main/java/com/robotobos/beanbrew/MainActivity.java
@@ -3,12 +3,61 @@ package com.robotobos.beanbrew;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Socket socket;
+
+    private static final int SERVERPORT = 8334;
+    private static final String SERVER_IP = "192.168.1.109";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Thread(new MainActivity.ClientThread()).start();
+    }
+
+    /** Called when the user taps the Send button */
+    public void sendMessage(View view) {
+        try {
+            EditText et = (EditText) findViewById(R.id.message);
+            String str = et.getText().toString();
+            PrintWriter out = new PrintWriter(new BufferedWriter(
+                    new OutputStreamWriter(socket.getOutputStream())),
+                    true);
+            out.println(str);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    class ClientThread implements Runnable {
+
+        @Override
+        public void run() {
+
+            try {
+                InetAddress serverAddr = InetAddress.getByName(SERVER_IP);
+                socket = new Socket(serverAddr, SERVERPORT);
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+
+        }
+
     }
 }
+
--- brewControl/onTestClient.py
+++ brewControl/onTestClient.py
@@ -3,6 +3,6 @@ import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(('192.168.1.109', 8334))
 
-s.send(bytes("coffee_pot_off","utf-8"))
+s.send(bytes("coffee_pot_on","utf-8"))
 
 s.close
